49a1496e85c28a5e284e3d2c4faa1006a0105726
keithbloomfield <kthblmfld@gmail.com>
2018-11-14 21:31:47 -0800

    Moves ldap_sync into listener thread Defines LdapValidationException Separates db connect try/catch from record processing ... Adds initial validation to ldap message (todo: share with outbound_sync) Removes redundant db connector from ldap_sync Adds detection/import of target provider
    Signed-off-by: keithbloomfield <kthblmfld@gmail.com>

--- rbac/providers/ldap/outbound_queue_listener.py
+++ rbac/providers/ldap/outbound_queue_listener.py
@@ -13,15 +13,18 @@
 # limitations under the License.
 # ------------------------------------------------------------------------------
 
-import sys
-import time
 import logging
+import time
+import os
+
 import rethinkdb as r
 from tornado import gen
 
+from rbac.providers.error.validation import LdapValidationException
+from rbac.providers.ldap.outbound_sync import process_entry
+from rbac.providers.ldap.ldap_message_validator import validate
+
 LOGGER = logging.getLogger(__name__)
-LOGGER.level = logging.DEBUG
-LOGGER.addHandler(logging.StreamHandler(sys.stdout))
 
 DB_HOST = "rethink"
 DB_PORT = 28015
@@ -29,26 +32,50 @@ DB_NAME = "rbac"
 DB_TABLE = "queue_outbound"
 RETRY_INTERVAL_SECONDS_TABLE_READY = 3
 
+ENV_VAR_MESSAGE_TARGET = "LDAP_DC"
+MESSAGE_TARGET_KEY_LDAP = "provider_id"
+MESSAGE_TARGET_VALUE_INVALID = "missing-env-target"
+MESSAGE_TARGET_VALUE_LDAP = os.getenv(ENV_VAR_MESSAGE_TARGET)
+
 r.set_loop_type("tornado")
 
 
 @gen.coroutine
-def print_feed_change_data():
-    connected = False
+def export_feed_change_to_ldap():
+    """Fetches changes from the outbound queue table, validates and publishes them to Ldap"""
 
-    while not connected:
-        try:
-            connection = yield r.connect(DB_HOST, DB_PORT, DB_NAME)
-            feed = yield r.table(DB_TABLE).changes().run(connection)
-            connected = True
-            while (yield feed.fetch_next()):
-                item = yield feed.next()
-                LOGGER.debug(item)
-        except r.ReqlRuntimeError as e:
-            LOGGER.info(
-                "Attempt to connect to %s threw exception: %s. Retrying in %s seconds",
-                DB_TABLE,
-                str(e),
-                RETRY_INTERVAL_SECONDS_TABLE_READY,
-            )
-            time.sleep(RETRY_INTERVAL_SECONDS_TABLE_READY)
+    connected = False
+    feed = None
+    rethink_header = "new_val"
+
+    if MESSAGE_TARGET_VALUE_LDAP == MESSAGE_TARGET_VALUE_INVALID:
+        LOGGER.warning("No message target found on the environment for outbound Ldap (key = %s). Shutting down listener",
+                       ENV_VAR_MESSAGE_TARGET)
+    else:
+        while not connected:
+            try:
+                connection = yield r.connect(DB_HOST, DB_PORT, DB_NAME)
+                feed = yield r.table(DB_TABLE).filter(
+                    {MESSAGE_TARGET_KEY_LDAP: "DC=clouddev,DC=corporate,DC=t-mobile,DC=com"}).changes().run(connection)
+                connected = True
+            except r.ReqlRuntimeError as re:
+                LOGGER.info(
+                    "Attempt to connect to %s threw exception: %s. Retrying in %s seconds",
+                    DB_TABLE,
+                    str(re),
+                    RETRY_INTERVAL_SECONDS_TABLE_READY,
+                )
+                time.sleep(RETRY_INTERVAL_SECONDS_TABLE_READY)
+
+        while (yield feed.fetch_next()):
+            new_record = yield feed.next()
+            content = new_record[rethink_header]
+
+            try:
+                validate(content)
+                process_entry(content, r)
+            except LdapValidationException as le:
+                # TODO: Determine what to do with inadequate ldap data in the queue. Log and drop?
+                LOGGER.error(
+                    "Ldap payload: %s encountered a validation error: %s", content, le
+                )
---
+++ tests/unit/providers/ldap/ldap_message_validator_test.py
@@ -0,0 +1,68 @@
+# Copyright 2018 Contributors to Hyperledger Sawtooth
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------------------
+
+from rbac.providers.ldap import ldap_message_validator
+from rbac.providers.error.validation import LdapValidationException
+
+
+def test_validate_missing_data_type():
+    """ensures a failure occurs when 'data_type' field is missing"""
+    ldap_payload = {"data": {"distinguished_name": "yo"}}
+
+    try:
+        ldap_message_validator.validate(ldap_payload)
+    except LdapValidationException as lde:
+        assert str(lde) == "Required field: 'data_type' is missing"
+
+
+def test_validate_missing_data_field():
+    """ensures a failure occurs when 'data' field is missing"""
+    ldap_payload = {"data_type": "user"}
+
+    try:
+        ldap_message_validator.validate(ldap_payload)
+    except LdapValidationException as lde:
+        assert str(lde) == "Required field: 'data' is missing"
+
+
+def test_validate_invalid_data_type():
+    """ensures a failure occurs when 'data_type' field is invalid"""
+    ldap_payload = {"data": {"distinguished_name": "yo"}, "data_type": "no"}
+
+    try:
+        ldap_message_validator.validate(ldap_payload)
+    except LdapValidationException as lde:
+        assert (str(lde) == "Invalid value for 'data_type'. 'data_type' must be in: ['user', 'group']"
+                )
+
+
+def test_validate_missing_distinguished_name():
+    """ensures a failure occurs when 'distinguished_name' field is missing"""
+    ldap_payload = {"data": {}, "data_type": "user"}
+
+    try:
+        ldap_message_validator.validate(ldap_payload)
+    except LdapValidationException as lde:
+        assert str(lde) == "'data' is missing an entry for: 'distinguished_name'"
+
+
+def test_validate_empty_distinguished_name():
+    """ensures a failure occurs when 'distinguished_name' field is empty"""
+    ldap_payload = {"data": {"distinguished_name": ""}, "data_type": "user"}
+
+    try:
+        ldap_message_validator.validate(ldap_payload)
+    except LdapValidationException as lde:
+        assert str(lde) == "'data'.'distinguished_name' cannot be empty"
